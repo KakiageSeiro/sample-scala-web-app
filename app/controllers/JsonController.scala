6c0f8b02d68c01c87c45b95391be2b7663b45dfb
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2024-05-04 08:51:27 +0900

    JsonAPIのサンプルにPOST追加

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -3,6 +3,7 @@ package controllers
 import play.api.mvc.{BaseController, *}
 import play.api.libs.json.*
 import play.api.libs.functional.syntax.*
+import play.api.libs.json.Reads._
 
 import javax.inject.*
 
@@ -13,18 +14,59 @@ class JsonController @Inject()(val controllerComponents: ControllerComponents) e
     val json = Json.toJson(Place.list)
     Ok(json)
   }
+
+  def savePlace(): Action[JsValue] = Action(parse.json) { request =>
+    val placeResult = request.body.validate[Place]
+    placeResult.fold(
+      errors => {
+        BadRequest(Json.obj("message" -> JsError.toJson(errors)))
+      },
+      place => {
+        Place.save(place)
+        Ok(Json.obj("message" -> ("Place '" + place.name + "' saved.")))
+      }
+    )
+  }
+
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
+
+  // jsonのパースとバリデーション
+  // バリデーションに失敗した場合はEitherを返す
+  def validateJson[A: Reads]: BodyParser[A] = parse.json.validate(
+    _.validate[A].asEither.left.map(e => BadRequest(JsError.toJson(e)))
+  )
+
+  // バリデーションをしたくないときは引数のvalidateJson[Place]をBodyParsers.parse.json[Place]で置き換える
+  def savePlaceConcise: Action[Place] = Action(validateJson[Place]) { request =>
+    // request.bodyにはバリデーション済のPlaceが入ってる
+    val place = request.body
+
+    Place.save(place)
+    Ok(Json.obj("message" -> ("Place '" + place.name + "' saved.")))
+  }
 }
 
-// Location=>Json
+// Location => Json
 implicit val locationWrites: Writes[Location] =
   (JsPath \ "lat").write[Double]
     .and((JsPath \ "long").write[Double])(unlift(Location.unapply))
 
-// Place=>Json
+// Place => Json
 implicit val placeWrites: Writes[Place] =
   (JsPath \ "name")
     .write[String]
     .and((JsPath \ "location").write[Location])(unlift(Place.unapply))
+
+// Json => Location
+// readのうしろににバリデーションでつかう条件を追加してるよ
+implicit val locationReads: Reads[Location] =
+  (JsPath \ "lat")
+    .read[Double](min(-90.0).keepAnd(max(90.0)))
+    .and((JsPath \ "long").read[Double](min(-180.0).keepAnd(max(180.0))))(Location.apply _)
+
+// Json => Place
+implicit val placeReads: Reads[Place] =
+  (JsPath \ "name").read[String](minLength[String](2)).and((JsPath \ "location").read[Location])(Place.apply _)
 
 case class Location(lat: Double, long: Double)
 object Location {
