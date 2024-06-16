f48e22dde3e3c0e13b00404409feaec0daa2b5a2
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2024-06-16 09:00:43 +0900

    Pekkoにいくぞ！

--- test/command/sample/controllers/JsonControllerSpec.scala
+++ test/command/sample/controllers/JsonControllerSpec.scala
@@ -1,21 +1,12 @@
 package command.sample.controllers
 
+import command.sample.infrastructure.DBAccessSampleDataSource
 import org.scalatestplus.play.*
+import org.scalatestplus.play.guice.*
 import play.api.mvc.*
 import play.api.test.*
 import play.api.test.Helpers.*
-import org.scalatestplus.play.guice.*
 
-import command.sample.infrastructure.DBAccessSampleDataSource
-import org.apache.pekko.util.ByteString
-import play.api.libs.json.{JsValue, Json}
-import play.api.libs.streams.Accumulator
-
-import org.scalatestplus.play._
-import org.scalatestplus.play.guice._
-import play.api.libs.json.Json
-import play.api.test._
-import play.api.test.Helpers._
 import scala.concurrent.Future
 
 class JsonControllerSpec extends PlaySpec with Results with GuiceOneAppPerTest {
@@ -27,5 +18,4 @@ class JsonControllerSpec extends PlaySpec with Results with GuiceOneAppPerTest {
       bodyText mustBe """[{"name":"Sandleford","location":{"lat":51.377797,"long":-1.318965}},{"name":"Watership Down","location":{"lat":51.235685,"long":-1.309197}}]"""
     }
   }
-
 }
